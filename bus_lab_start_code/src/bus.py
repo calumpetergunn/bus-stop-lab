c43c7a2e455af6c0219aed396b4c1078ad646e67
calumpetergunn <calumgunn@gmail.com>
2021-03-03 10:03:21 +0000

    amended final problem

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -21,7 +21,8 @@ class Bus:
     def empty(self):
         self.passengers.clear()
 
-    def pick_up_from_stop(self, bus_stop):
-        self.passengers += bus_stop.queue
-        bus_stop.queue.clear
+    def pick_up_from_stop(self, bus_stop_to_pick_up_from):
+        for passenger in bus_stop_to_pick_up_from.queue:
+            self.passengers.append(passenger)
         
+
